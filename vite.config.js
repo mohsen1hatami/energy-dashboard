bc3add1b041ce7b227020f1b898d9a5d8b6d9d7d
mohsen1hatami <hatami.mohsen@gmail.com>
2026-09-15 06:34:50 +0330

    add real site build endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], base: "/energy-dashboard/", });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
